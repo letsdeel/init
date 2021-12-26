92832794b88bb086b1d1d9f6240e2ba583c886e2
Gilad Novik <gilad@novik.ca>
2021-12-26 19:12:07 +0200

    ASYNC_CONTEXT flag

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 require('axios/lib/core/createError');
 const fs = require('fs');
-const {AsyncLocalStorage} = require('async_hooks');
+const async_hooks = require('async_hooks');
 
 try {
     Object.assign(process.env, Object.assign(JSON.parse(fs.readFileSync('/var/secrets/secrets.json', 'utf8')), process.env));
@@ -14,8 +14,8 @@ global.log = require('pino')({
     level: String(process.env.LOG_LEVEL || 'warn').toLowerCase(),
     formatters: {level: (level) => ({level: level.toUpperCase()})},
     mixin: () => ({
-        requestId: global.asyncContext?.requestId,
-        cronTask: global.asyncContext?.cronTask,
+        requestId: process.env.ASYNC_CONTEXT ? global.asyncContext?.requestId : global.currentContext?.requestId,
+        cronTask: process.env.ASYNC_CONTEXT ? global.asyncContext?.cronTask : global.currentContext?.cronTask,
     }),
     base: undefined,
     ...(global.__DEV__
@@ -32,13 +32,31 @@ if (process.env.S3_ENDPOINT) {
     AWS.config.update({endpoint: new AWS.Endpoint(process.env.S3_ENDPOINT), s3ForcePathStyle: true, signatureVersion: 'v4'});
 }
 
-const context = new AsyncLocalStorage();
+if (process.env.ASYNC_CONTEXT) {
+    const context = new async_hooks.AsyncLocalStorage();
 
-Object.defineProperty(global, 'asyncContext', {
-    get() {
-        return context.getStore() || context.run.bind(context);
-    },
-});
+    Object.defineProperty(global, 'asyncContext', {
+        get() {
+            return context.getStore() || context.run.bind(context);
+        },
+    });
+} else {
+    const contexts = {};
+
+    async_hooks
+        .createHook({
+            init: (asyncId, type, triggerAsyncId) => contexts[triggerAsyncId] && (contexts[asyncId] = contexts[triggerAsyncId]),
+            destroy: (asyncId) => delete contexts[asyncId],
+        })
+        .enable();
+
+    Object.defineProperty(global, 'currentContext', {
+        get() {
+            const asyncId = async_hooks.executionAsyncId();
+            return contexts[asyncId] || (contexts[asyncId] = {});
+        },
+    });
+}
 
 global.Sentry = require('@sentry/node');
 
@@ -58,5 +76,3 @@ for (const m of module.children) {
     };
     break;
 }
-
-module.exports = {context};
